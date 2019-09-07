0a07b4293579ba3e6151220bc310714e8986fcbf
Rebeca <rmendizabal4@gmail.com>
2019-09-07 12:43:18 -0500

    #28 Generación del PWM
    Configuración del PWM usando como dutyCycle el valor mapeado del ADC.

--- CODIGO/Workspace_Proyecto_Integrador/Controlador_Motor/src/hal_entry.c
+++ CODIGO/Workspace_Proyecto_Integrador/Controlador_Motor/src/hal_entry.c
@@ -7,13 +7,25 @@ uint16_t u16ADC_Data, FilteredData;
 
 void hal_entry(void)
 {
+    /* ADC configuration*/
     g_adc0.p_api->open(g_adc0.p_ctrl, g_adc0.p_cfg);
     g_adc0.p_api->scanCfg(g_adc0.p_ctrl, g_adc0.p_channel_cfg);
     g_adc0.p_api->scanStart(g_adc0.p_ctrl);
 
+    /* PWM configuration*/
+    g_timer1.p_api->open(g_timer1.p_ctrl, g_timer1.p_cfg);
+    g_timer1.p_api->start(g_timer1.p_ctrl);
+
     while(1)
     {
+        /* Read ADC value*/
         g_adc0.p_api->read(g_adc0.p_ctrl, ADC_REG_CHANNEL_0, &u16ADC_Data);
         FilteredData = (u16ADC_Data * 100)/255;
+
+        /* Change PWM dutyCycle*/
+        g_timer1.p_api->open(g_timer1.p_ctrl, g_timer1.p_cfg);
+        g_timer1.p_api->start(g_timer1.p_ctrl);
+        g_timer1.p_api->dutyCycleSet(g_timer1.p_ctrl, FilteredData, TIMER_PWM_UNIT_PERCENT, 1);
+
     }
 }
--- CODIGO/Workspace_Proyecto_Integrador/Controlador_Motor/src/synergy_gen/hal_data.c
+++ CODIGO/Workspace_Proyecto_Integrador/Controlador_Motor/src/synergy_gen/hal_data.c
@@ -1,6 +1,25 @@
 /* generated HAL source file - do not edit */
 #include "hal_data.h"
 #if (BSP_IRQ_DISABLED) != BSP_IRQ_DISABLED
+#if !defined(SSP_SUPPRESS_ISR_g_timer1) && !defined(SSP_SUPPRESS_ISR_GPT1)
+SSP_VECTOR_DEFINE_CHAN(gpt_counter_overflow_isr, GPT, COUNTER_OVERFLOW, 1);
+#endif
+#endif
+static gpt_instance_ctrl_t g_timer1_ctrl;
+static const timer_on_gpt_cfg_t g_timer1_extend =
+{ .gtioca =
+{ .output_enabled = true, .stop_level = GPT_PIN_LEVEL_LOW },
+  .gtiocb =
+  { .output_enabled = true, .stop_level = GPT_PIN_LEVEL_LOW } };
+static const timer_cfg_t g_timer1_cfg =
+{ .mode = TIMER_MODE_PWM, .period = 100, .unit = TIMER_UNIT_FREQUENCY_HZ, .duty_cycle = 30, .duty_cycle_unit =
+          TIMER_PWM_UNIT_PERCENT,
+  .channel = 1, .autostart = true, .p_callback = NULL, .p_context = &g_timer1, .p_extend = &g_timer1_extend, .irq_ipl =
+          (BSP_IRQ_DISABLED), };
+/* Instance structure to use this module. */
+const timer_instance_t g_timer1 =
+{ .p_ctrl = &g_timer1_ctrl, .p_cfg = &g_timer1_cfg, .p_api = &g_timer_on_gpt };
+#if (BSP_IRQ_DISABLED) != BSP_IRQ_DISABLED
 #if !defined(SSP_SUPPRESS_ISR_g_adc0) && !defined(SSP_SUPPRESS_ISR_ADC0)
 SSP_VECTOR_DEFINE_CHAN(adc_scan_end_isr, ADC, SCAN_END, 0);
 #endif
